ebc4458883310513ded4a7b83729f741d560f225
Max Schram <maxbschram@gmail.com>
2015-10-21 15:37:00 -0700

    Add authenticate by token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+
+  def authenticate_request
+    begin
+      token = JWT.decode(request.headers['Authorization'], 
+                         Rails.application.secrets.secret_key_base)
+      payload = token[0]
+      @current_user = User.find(id: payload['uid'])
+    rescue JWT::DecodeError
+      render json: 'authentication failed, invalid token', status: 401
+    end
+  end
 end
